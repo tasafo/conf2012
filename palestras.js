ed931435eaa4ba27a63d7396daf66c3afba3c169
Paulo Moura <Paulo Moura>
2012-04-27 00:56:21 -0300

    aleteracao de grade

---
+++ palestras.js
@@ -0,0 +1,10 @@
+$(function(){
+	$("#trigger-rildo, #trigger-caike, #trigger-sandro-marcos,#trigger-edgar, #trigger-magno, #trigger-manoel, #trigger-cardoso, #trigger-aldrin, #trigger-ricardo, #trigger-papo, #trigger-jaime")
+	.fancybox({
+        'width'             : '50%',
+        'height'            : '65%',
+        'autoScale'         : false,
+        'transitionIn'      : 'none',
+        'transitionOut'     : 'none'
+    });
+})
